f75e2f381c6234641225ca7c2f403fc15027e2df
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2019-11-03 18:54:24 -0300

    Add files via upload

--- R/anlaise_dados.R
+++ R/anlaise_dados.R
@@ -83,8 +83,9 @@ df_defeso %>%
       ) %>%  
       mutate(grupo= "Demais Cidades NE") 
   )%>%
-ggplot(aes(x= grupo, y= log(soma_valor))) +
-  geom_boxplot(aes(fill=grupo))
+ggplot(aes(x= grupo, y= soma_valor)) +
+  geom_boxplot(aes(fill=grupo)) +
+  scale_y_log10()
 
 
 
@@ -97,55 +98,61 @@ df_dca_mun_sel_func%>%
   filter(cod_interno=="18",
          coluna=="Despesas Liquidadas") %>%
   mutate(grupo="Cidades afetadas",
-         desp_per_capta= valor/populacao)%>%
+         desp_per_capita= valor/populacao)%>%
   bind_rows(
     df_dca_NE_func%>%
       filter(cod_interno=="18",
              coluna=="Despesas Liquidadas") %>%
       anti_join(df_dca_mun_sel_func) %>%
       mutate(grupo= "Demais Cidades NE",
-             desp_per_capta = valor/populacao)
+             desp_per_capita = valor/populacao)
   ) %>%
-  ggplot(aes(x= grupo, y= log(desp_per_capta))) +
-  geom_boxplot(aes(fill=grupo))
+  ggplot(aes(x= grupo, y= desp_per_capita)) +
+  geom_boxplot(aes(fill=grupo))+
+  scale_y_log10()
 
 
 df_dca_mun_sel_func%>%
   filter(cod_interno=="10",
          coluna=="Despesas Liquidadas") %>%
   mutate(grupo="Cidades afetadas",
-         desp_per_capta= valor/populacao)%>%
+         desp_per_capita= valor/populacao)%>%
   bind_rows(
     df_dca_NE_func%>%
       filter(cod_interno=="10",
              coluna=="Despesas Liquidadas") %>%
       anti_join(df_dca_mun_sel_func) %>%
       mutate(grupo= "Demais Cidades NE",
-             desp_per_capta = valor/populacao)
+             desp_per_capita = valor/populacao)
   ) %>%
-  ggplot(aes(x= grupo, y= log(desp_per_capta))) +
-  geom_boxplot(aes(fill=grupo))
+  ggplot(aes(x= grupo, y= desp_per_capita)) +
+  geom_boxplot(aes(fill=grupo))+
+  scale_y_log10()
 
 
 df_dca_mun_sel_func%>%
   filter(cod_interno=="23.695",
          coluna=="Despesas Liquidadas") %>%
   mutate(grupo="Cidades afetadas",
-         desp_per_capta= valor/populacao)%>%
+         desp_per_capita= valor/populacao)%>%
   bind_rows(
     df_dca_NE_func%>%
       filter(cod_interno=="23.695",
              coluna=="Despesas Liquidadas") %>%
       anti_join(df_dca_mun_sel_func) %>%
       mutate(grupo= "Demais Cidades NE",
-             desp_per_capta = valor/populacao)
+             desp_per_capita = valor/populacao)
   ) %>%
-  ggplot(aes(x= grupo, y= log(desp_per_capta))) +
-  geom_boxplot(aes(fill=grupo))
+  ggplot(aes(x= grupo, y= desp_per_capita)) +
+  geom_boxplot(aes(fill=grupo)) +
+  scale_y_log10()
 
 
 library(tidyr)
 library(stringr)
+
+1.7.0.0.00.0.0
+2.4.0.0.00.0.0
 
 df_dca_mun_sel_RO_tidy<-
 df_dca_mun_sel_RO %>% 
@@ -154,14 +161,107 @@ df_dca_mun_sel_RO %>%
   spread(coluna, valor) %>%
   rename_at(3:6,str_remove_all," ") %>%
   rename_at(3:6,str_remove_all,"-") %>%
-  mutate(receita_liquida = ReceitasBrutasRealizadas- 
+  mutate(
+    tipo_receita = "receita_liquida_total",
+    receita_liquida = ReceitasBrutasRealizadas- 
                            ifelse(is.na(DeduçõesFUNDEB),0,DeduçõesFUNDEB) -
                            ifelse(is.na(DeduçõesTransferênciasConstitucionais),0,DeduçõesTransferênciasConstitucionais)-
-                           ifelse(is.na(OutrasDeduçõesdaReceita),0,OutrasDeduçõesdaReceita))
+                           ifelse(is.na(OutrasDeduçõesdaReceita),0,OutrasDeduçõesdaReceita)) %>%
+  select(1,7,8) %>%
+bind_rows(
+  df_dca_mun_sel_RO %>% 
+    filter(cod_interno== "1.7.0.0.00.0.0")%>%
+    select(cod_ibge, coluna, cod_interno, valor) %>%
+    spread(coluna, valor)%>%
+    rename_at(3:6,str_remove_all," ") %>%
+    rename_at(3:6,str_remove_all,"-") %>%
+    mutate(
+      tipo_receita = "transf_corrente_liq",
+      receita_liquida = ReceitasBrutasRealizadas- 
+             ifelse(is.na(DeduçõesFUNDEB),0,DeduçõesFUNDEB) -
+             ifelse(is.na(DeduçõesTransferênciasConstitucionais),0,DeduçõesTransferênciasConstitucionais)-
+             ifelse(is.na(OutrasDeduçõesdaReceita),0,OutrasDeduçõesdaReceita)) %>%
+    select(1,7,8) 
+)%>%
+  bind_rows(
+    df_dca_mun_sel_RO %>% 
+      filter(cod_interno== "2.4.0.0.00.0.0")%>%
+      select(cod_ibge, coluna, cod_interno, valor) %>%
+      spread(coluna, valor)%>%
+      rename_at(3:4,str_remove_all," ") %>%
+      rename_at(3:4,str_remove_all,"-") %>%
+      mutate(
+        tipo_receita = "transf_capital_liq",
+        receita_liquida = ReceitasBrutasRealizadas- 
+          ifelse(is.na(OutrasDeduçõesdaReceita),0,OutrasDeduçõesdaReceita)) %>%
+      select(1,5,6)
+  ) %>%
+  spread(tipo_receita,receita_liquida) %>%
+  mutate(transf_capital_liq= ifelse(is.na(transf_capital_liq),0,transf_capital_liq),
+         indice_dependencia = (transf_capital_liq+transf_corrente_liq)/receita_liquida_total)
+  
+
+df_dca_NE_RO_tidy<-
+  df_dca_NE_RO %>% 
+  filter(cod_interno== "TotalReceitas")%>%
+  select(cod_ibge, coluna, cod_interno, valor) %>%
+  spread(coluna, valor) %>%
+  rename_at(3:6,str_remove_all," ") %>%
+  rename_at(3:6,str_remove_all,"-") %>%
+  mutate(
+    tipo_receita = "receita_liquida_total",
+    receita_liquida = ReceitasBrutasRealizadas- 
+      ifelse(is.na(DeduçõesFUNDEB),0,DeduçõesFUNDEB) -
+      ifelse(is.na(DeduçõesTransferênciasConstitucionais),0,DeduçõesTransferênciasConstitucionais)-
+      ifelse(is.na(OutrasDeduçõesdaReceita),0,OutrasDeduçõesdaReceita)) %>%
+  select(1,7,8) %>%
+  bind_rows(
+    df_dca_NE_RO %>% 
+      filter(cod_interno== "1.7.0.0.00.0.0")%>%
+      select(cod_ibge, coluna, cod_interno, valor) %>%
+      spread(coluna, valor)%>%
+      rename_at(3:6,str_remove_all," ") %>%
+      rename_at(3:6,str_remove_all,"-") %>%
+      mutate(
+        tipo_receita = "transf_corrente_liq",
+        receita_liquida = ReceitasBrutasRealizadas- 
+          ifelse(is.na(DeduçõesFUNDEB),0,DeduçõesFUNDEB) -
+          ifelse(is.na(DeduçõesTransferênciasConstitucionais),0,DeduçõesTransferênciasConstitucionais)-
+          ifelse(is.na(OutrasDeduçõesdaReceita),0,OutrasDeduçõesdaReceita)) %>%
+      select(1,7,8) 
+  )%>%
+  bind_rows(
+    df_dca_NE_RO %>% 
+      filter(cod_interno== "2.4.0.0.00.0.0")%>%
+      select(cod_ibge, coluna, cod_interno, valor) %>%
+      spread(coluna, valor)%>%
+      rename_at(3:6,str_remove_all," ") %>%
+      rename_at(3:6,str_remove_all,"-") %>%
+      mutate(
+        tipo_receita = "transf_capital_liq",
+        receita_liquida = ReceitasBrutasRealizadas- 
+          ifelse(is.na(DeduçõesFUNDEB),0,DeduçõesFUNDEB) -
+          ifelse(is.na(DeduçõesTransferênciasConstitucionais),0,DeduçõesTransferênciasConstitucionais)-
+          ifelse(is.na(OutrasDeduçõesdaReceita),0,OutrasDeduçõesdaReceita)) %>%
+      select(1,7,8)
+  ) %>%
+  spread(tipo_receita,receita_liquida) %>%
+  mutate(transf_capital_liq= ifelse(is.na(transf_capital_liq),0,transf_capital_liq),
+         indice_dependencia = (transf_capital_liq+transf_corrente_liq)/receita_liquida_total)
 
 
+df_dca_mun_sel_RO_tidy %>%
+  mutate(grupo="Cidades afetadas") %>%
+bind_rows(
+  df_dca_NE_RO_tidy %>%
+    anti_join(df_dca_mun_sel_RO_tidy) %>%
+    mutate(grupo="Demais Cidades NE") 
+) %>%
+  ggplot()+
+  geom_boxplot(aes(x=grupo, y=indice_dependencia, fill= grupo))
 
-names(PIB_Mun_sel)[43:45]<-c("PIB_1","PIB_2","PIB_3")
+
+names(PIB_Mun_sel)[40:45]<-c("PIB",  "População", "PIB_per_capita", "PIB_1","PIB_2","PIB_3")
 names(PIB_Mun_NE)[43:45]<-c("PIB_1","PIB_2","PIB_3")
 
 PIB_Mun_sel %>%
@@ -201,8 +301,44 @@ PIB_Mun_NE %>%
     TRUE                       ~ "Pesca não pertence ao PIB 3"
   )) %>%
   ggplot(aes(x=grupo)) +
+  geom_point(aes(x=população, y=PIB, color=grupo))
+
+PIB_Mun_sel %>%
+  filter(Ano==2016) %>%
+  select(codigo_municipio, 40:45) %>%
+  mutate(grupo =case_when(
+    str_count(PIB_1,"pesca")>0 ~ "Pesca PIB 3",
+    str_count(PIB_2,"pesca")>0 ~ "Pesca PIB 3",
+    str_count(PIB_3,"pesca")>0 ~ "Pesca PIB 3",
+    TRUE                       ~ "Pesca não pertence ao PIB 3"
+  )) %>%
+  ggplot() +
+  geom_point(aes(x=População, y=PIB, color=grupo))+
+  scale_x_log10()+
+  scale_y_log10()
+
+  
+  
+    geom_bar()+
+  coord_flip()
+
+
+
+PIB_Mun_NE %>%
+  filter(Ano==2016) %>%
+  select(codigo_municipio, 40:45) %>%
+  mutate(grupo =case_when(
+    str_count(PIB_1,"pesca")>0 ~ "Pesca principal PIB",
+    str_count(PIB_2,"pesca")>0 ~ "Pesca segundo PIB",
+    str_count(PIB_3,"pesca")>0 ~ "PIB terceiro PIB",
+    TRUE                       ~ "Pesca não pertence ao PIB 3"
+  )) %>%
+  ggplot(aes(x=grupo)) +
   geom_bar()+
   coord_flip()
+
+
+
 
 
     
