f4c96b5da8d714a980e887f08bdaecad5ef3650e
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2019-11-05 15:07:17 -0300

    Add files via upload

--- R/anlaise_dados.R
+++ R/anlaise_dados.R
@@ -5,6 +5,10 @@ library(dplyr)
 library(stringr)
 library(lares)
 
+library(abjutils)
+
+
+
 
 ###Gráficos diversos para exploração de dados
 
@@ -74,7 +78,7 @@ df_defeso %>%
 
 df_defeso %>% 
   semi_join(ibama) %>%
-  group_by(Municipio) %>%
+  group_by(uf, Municipio) %>%
   summarise(
     soma_valor = sum(valor_parcela)
   ) %>% 
@@ -86,16 +90,17 @@ df_defeso %>%
                     distinct(uf))$uf)
       ) %>%
       anti_join(ibama) %>%
-      group_by(Municipio) %>%
+      group_by(uf, Municipio) %>%
       summarise(
         soma_valor = sum(valor_parcela)
       ) %>%  
       mutate(grupo= "Demais Cidades NE") 
   )%>%
-ggplot(aes(x= grupo, y= soma_valor)) +
-  geom_boxplot(aes(fill=grupo)) +
-  scale_y_log10(labels=function(x) format(x, big.mark = ".", scientific = FALSE)) +
+  ggplot(aes(x= grupo, y= soma_valor)) +
+  geom_violin(aes(fill=grupo)) +
+  scale_y_log10(labels=function(x) format(x, big.mark = ".", scientific = FALSE),minor_breaks= NULL, breaks= c(10^3,10^4,10^5,1.3*10^6,10^7)) +
   theme_light()+
+  theme(panel.border = element_blank())+
   labs(x="",
        y="Valor total em R$")
 
@@ -117,11 +122,13 @@ df_dca_mun_sel_func%>%
              desp_per_capita = valor/populacao)
   ) %>%
   ggplot(aes(x= grupo, y= desp_per_capita)) +
-  geom_boxplot(aes(fill=grupo)) +
-  scale_y_log10(labels=function(x) format(x, big.mark = ".", scientific = FALSE)) +
+  geom_violin(aes(fill=grupo)) +
+  scale_y_log10(labels=function(x) format(x, decimal.mark=",", big.mark = ".", scientific = FALSE),minor_breaks= NULL, breaks=c(0.1,8,12,70,270,1000)) +
   theme_light()+
+  theme(panel.border = element_blank())+
   labs(x="",
        y="Valor por habitante em R$")
+
 
 
 ### Gráfico de despesa com saúde
@@ -141,9 +148,10 @@ df_dca_mun_sel_func%>%
              desp_per_capita = valor/populacao)
   ) %>%
   ggplot(aes(x= grupo, y= desp_per_capita)) +
-  geom_boxplot(aes(fill=grupo))+
-  scale_y_log10(labels=function(x) format(x, big.mark = ".", scientific = FALSE)) +
+  geom_violin(aes(fill=grupo))+
+  scale_y_log10(labels=function(x) format(x, big.mark = ".", scientific = FALSE), breaks= c(100,300,560,1000,3000)) +
   theme_light()+
+  theme(panel.border = element_blank())+
   labs(x="",
        y="Valor por habitante em R$")
 
@@ -164,9 +172,10 @@ df_dca_mun_sel_func%>%
              desp_per_capita = valor/populacao)
   ) %>%
   ggplot(aes(x= grupo, y= desp_per_capita)) +
-  geom_boxplot(aes(fill=grupo)) +
-  scale_y_log10(labels=function(x) format(x, big.mark = ".", scientific = FALSE)) +
+  geom_violin(aes(fill=grupo))+
+  scale_y_log10(labels=function(x) format(x,decimal.mark=",", big.mark = ".", scientific = FALSE), breaks= c(0.01,1,10,15,100), minor_breaks= NULL ) +
   theme_light()+
+  theme(panel.border = element_blank())+
   labs(x="",
        y="Valor por habitante em R$")
 
@@ -177,7 +186,7 @@ library(tidyr)
 
 
 df_dca_mun_sel_RO_tidy<-
-df_dca_mun_sel_RO %>% 
+  df_dca_mun_sel_RO %>% 
   filter(cod_interno== "TotalReceitas")%>%
   select(cod_ibge, coluna, cod_interno, valor) %>%
   spread(coluna, valor) %>%
@@ -186,25 +195,25 @@ df_dca_mun_sel_RO %>%
   mutate(
     tipo_receita = "receita_liquida_total",
     receita_liquida = ReceitasBrutasRealizadas- 
-                           ifelse(is.na(DeduçõesFUNDEB),0,DeduçõesFUNDEB) -
-                           ifelse(is.na(DeduçõesTransferênciasConstitucionais),0,DeduçõesTransferênciasConstitucionais)-
-                           ifelse(is.na(OutrasDeduçõesdaReceita),0,OutrasDeduçõesdaReceita)) %>%
+      ifelse(is.na(DeduçõesFUNDEB),0,DeduçõesFUNDEB) -
+      ifelse(is.na(DeduçõesTransferênciasConstitucionais),0,DeduçõesTransferênciasConstitucionais)-
+      ifelse(is.na(OutrasDeduçõesdaReceita),0,OutrasDeduçõesdaReceita)) %>%
   select(1,7,8) %>%
-bind_rows(
-  df_dca_mun_sel_RO %>% 
-    filter(cod_interno== "1.7.0.0.00.0.0")%>%
-    select(cod_ibge, coluna, cod_interno, valor) %>%
-    spread(coluna, valor)%>%
-    rename_at(3:6,str_remove_all," ") %>%
-    rename_at(3:6,str_remove_all,"-") %>%
-    mutate(
-      tipo_receita = "transf_corrente_liq",
-      receita_liquida = ReceitasBrutasRealizadas- 
-             ifelse(is.na(DeduçõesFUNDEB),0,DeduçõesFUNDEB) -
-             ifelse(is.na(DeduçõesTransferênciasConstitucionais),0,DeduçõesTransferênciasConstitucionais)-
-             ifelse(is.na(OutrasDeduçõesdaReceita),0,OutrasDeduçõesdaReceita)) %>%
-    select(1,7,8) 
-)%>%
+  bind_rows(
+    df_dca_mun_sel_RO %>% 
+      filter(cod_interno== "1.7.0.0.00.0.0")%>%
+      select(cod_ibge, coluna, cod_interno, valor) %>%
+      spread(coluna, valor)%>%
+      rename_at(3:6,str_remove_all," ") %>%
+      rename_at(3:6,str_remove_all,"-") %>%
+      mutate(
+        tipo_receita = "transf_corrente_liq",
+        receita_liquida = ReceitasBrutasRealizadas- 
+          ifelse(is.na(DeduçõesFUNDEB),0,DeduçõesFUNDEB) -
+          ifelse(is.na(DeduçõesTransferênciasConstitucionais),0,DeduçõesTransferênciasConstitucionais)-
+          ifelse(is.na(OutrasDeduçõesdaReceita),0,OutrasDeduçõesdaReceita)) %>%
+      select(1,7,8) 
+  )%>%
   bind_rows(
     df_dca_mun_sel_RO %>% 
       filter(cod_interno== "2.4.0.0.00.0.0")%>%
@@ -221,7 +230,7 @@ bind_rows(
   spread(tipo_receita,receita_liquida) %>%
   mutate(transf_capital_liq= ifelse(is.na(transf_capital_liq),0,transf_capital_liq),
          indice_dependencia = (transf_capital_liq+transf_corrente_liq)/receita_liquida_total)
-  
+
 
 df_dca_NE_RO_tidy<-
   df_dca_NE_RO %>% 
@@ -272,17 +281,19 @@ df_dca_NE_RO_tidy<-
          indice_dependencia = (transf_capital_liq+transf_corrente_liq)/receita_liquida_total)
 
 
+library(hrbrthemes)
 df_dca_mun_sel_RO_tidy %>%
   mutate(grupo="Cidades afetadas") %>%
-bind_rows(
-  df_dca_NE_RO_tidy %>%
-    anti_join(df_dca_mun_sel_RO_tidy) %>%
-    mutate(grupo="Demais Cidades NE") 
-) %>%
+  bind_rows(
+    df_dca_NE_RO_tidy %>%
+      anti_join(df_dca_mun_sel_RO_tidy) %>%
+      mutate(grupo="Demais Cidades NE") 
+  ) %>%
   ggplot()+
-  geom_boxplot(aes(x=grupo, y=indice_dependencia, fill= grupo))+
-  scale_y_percent()+
+  geom_violin(aes(x=grupo, y=indice_dependencia, fill= grupo))+
+  scale_y_percent(breaks=c(0.25,0.50,0.75,0.83,0.9,1), minor_breaks = NULL)+
   theme_light()+
+  theme(panel.border = element_blank())+
   labs(x="",
        y="Percentual de dependência")
 
@@ -293,10 +304,10 @@ names(PIB_Mun_sel)[c(8,40:45)]<-c("Municipio","PIB",  "População", "PIB_per_ca
 names(PIB_Mun_NE)[c(8,40:45)]<-c("Municipio","PIB",  "População", "PIB_per_capita", "PIB_1","PIB_2","PIB_3")
 
 ##Tratamento dos dados de seguro defeso
-PIB_Mun_sel$Municipio <- str_to_upper(cleanText(PIB_Mun_sel$Municipio))
+PIB_Mun_sel$Municipio <- str_to_upper(abjutils::rm_accent (PIB_Mun_sel$Municipio))
 
 
-PIB_Mun_NE$Municipio <- str_to_upper(cleanText(PIB_Mun_NE$Municipio))
+PIB_Mun_NE$Municipio <- str_to_upper(abjutils::rm_accent(PIB_Mun_NE$Municipio))
 
 #GRáficos de barra com comparação entre cidades com pesca que pertence ao PIB3
 df_defeso_NE<-
@@ -329,26 +340,26 @@ PIB_Mun_sel %>%
   filter(Ano==2016) %>%
   select(codigo_municipio, 43:45) %>%
   mutate( grupo_cidade = "Cidades Atingidas",
-    grupo_PIB =case_when(
-    str_count(PIB_1,"pesca")>0 ~ "Pesca PIB 3",
-    str_count(PIB_2,"pesca")>0 ~ "Pesca PIB 3",
-    str_count(PIB_3,"pesca")>0 ~ "Pesca PIB 3",
-    TRUE                       ~ "Pesca não pertence ao PIB 3"
-  )) %>%
-bind_rows(
-  PIB_Mun_NE %>%
-    anti_join(PIB_Mun_sel) %>%
-    filter(Ano==2016) %>%
-    select(codigo_municipio, 43:45) %>%
-    mutate(grupo_cidade = "Demais Cidades NE",
-           grupo =case_when(
-             str_count(PIB_1,"pesca")>0 ~ "Pesca PIB 3",
-             str_count(PIB_2,"pesca")>0 ~ "Pesca PIB 3",
-             str_count(PIB_3,"pesca")>0 ~ "Pesca PIB 3",
-             TRUE                       ~ "Pesca não pertence ao PIB 3"
-           ))
-)  %>% 
-   ggplot(aes(x=grupo, fill= grupo)) +
+          grupo_PIB =case_when(
+            str_count(PIB_1,"pesca")>0 ~ "Pesca PIB 3",
+            str_count(PIB_2,"pesca")>0 ~ "Pesca PIB 3",
+            str_count(PIB_3,"pesca")>0 ~ "Pesca PIB 3",
+            TRUE                       ~ "Pesca não pertence ao PIB 3"
+          )) %>%
+  bind_rows(
+    PIB_Mun_NE %>%
+      anti_join(PIB_Mun_sel) %>%
+      filter(Ano==2016) %>%
+      select(codigo_municipio, 43:45) %>%
+      mutate(grupo_cidade = "Demais Cidades NE",
+             grupo =case_when(
+               str_count(PIB_1,"pesca")>0 ~ "Pesca PIB 3",
+               str_count(PIB_2,"pesca")>0 ~ "Pesca PIB 3",
+               str_count(PIB_3,"pesca")>0 ~ "Pesca PIB 3",
+               TRUE                       ~ "Pesca não pertence ao PIB 3"
+             ))
+  )  %>% 
+  ggplot(aes(x=grupo, fill= grupo)) +
   geom_bar()+
   coord_flip()+
   scale_y_comma()+
@@ -370,7 +381,7 @@ PIB_Mun_sel %>%
     TRUE                       ~ "Pesca não pertence ao PIB 3"
   )) %>%
   ggplot() +
-  geom_point(aes(x=População, y=PIB_per_capita, color=grupo),alpha = 0.5)+
+  geom_point(aes(x=População, y=PIB_per_capita, color=grupo))+
   scale_x_log10(labels=function(x) format(x, big.mark = ".", scientific = FALSE))+
   scale_y_log10(labels=function(x) format(x, big.mark = ".", scientific = FALSE))+
   theme_light()+
@@ -382,29 +393,22 @@ PIB_Mun_sel %>%
 ###GRáfico de dispersão de população e renda per capita com os dois grupos de cidade
 
 PIB_Mun_sel %>%
+  filter(Ano==2016) %>%
+  select(codigo_municipio, 40:45) %>%
+  mutate( grupo = "Cidades atingidas") %>%
+  
+  bind_rows(
+    PIB_Mun_NE %>%
+      anti_join(PIB_Mun_sel) %>%
       filter(Ano==2016) %>%
       select(codigo_municipio, 40:45) %>%
-      mutate( grupo = "Cidades atingidas") %>%
-
-bind_rows(
-  PIB_Mun_NE %>%
-    anti_join(PIB_Mun_sel) %>%
-    filter(Ano==2016) %>%
-    select(codigo_municipio, 40:45) %>%
-    mutate( grupo = "Demais Cidades NE") 
-)  %>%
+      mutate( grupo = "Demais Cidades NE") 
+  )  %>%
   ggplot() +
   geom_point(aes(x=População, y=PIB_per_capita, color=grupo), alpha= 0.5)+
   scale_x_log10(labels=function(x) format(x, big.mark = ".", scientific = FALSE))+
   scale_y_log10(labels=function(x) format(x, big.mark = ".", scientific = FALSE))+
-                  theme_light()+
-                  labs(x="População",
-                       y="PIB per capita em R$")
-                
-
-  
-
-
-
-
-  
+  theme_light()+
+  theme(panel.border = element_blank())
+  labs(x="População",
+       y="PIB per capita em R$")
